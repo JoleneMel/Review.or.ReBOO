edfc392c94736518282c9e8273f786d26a111828
Jolene Mel <jolenemelanson33@gmail.com>
2021-03-19 21:18:40 -0400

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar'
+import Container from './components/container';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+// import MovieList from './components/movieList';
+import WebLayout from './components/web.layout';
+import React from 'react';
+let globalState = {
+  text: "Star",
+  id: 30127,
+  totalVotes: 0,
+};
+
+
+
+
+
+
 
 function App() {
   return (
+
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">"Review or ReBOO!!!"</header> */}
+      <body id="color">
+      <NavBar />,
+      <WebLayout />,
+      <Container globalState={globalState}/>,
+      
+      </body>
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+// import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <Provider >
+    <App />,
+// </Provider >,
   document.getElementById('root')
 );
 
